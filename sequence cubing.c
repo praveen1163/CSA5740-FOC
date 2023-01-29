691872b4f11fa507999d5dac6cb1c73c5f59e54b
praveen1163 <114171407+praveen1163@users.noreply.github.com>
2023-01-29 21:21:50 +0530

    Create sequence cubing.c

---
+++ sequence cubing.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main () {
+
+   int n,i;
+   printf("enter the number:");
+   scanf("%d",&n);
+   for(i= 1; i<=n;i++){
+      printf("%d\t", i*i*i);
+   }
+ 
+   return 0;
+}
+
